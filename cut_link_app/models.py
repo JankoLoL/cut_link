fbe0bc7fea4a88fb9e134da8e098455d726d3308
JankoLoL <adam.chch86@gmail.com>
2023-11-22 10:57:09 +0100

    model ShortenedUrl created with staticmethod

--- cut_link_app/models.py
+++ cut_link_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
+import string
+import random
 
-# Create your models here.
+
+class ShortenedUrl(models.Model):
+    original_url = models.URLField(max_length=2048)
+    shortened_id = models.CharField(max_length=10, unique=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='shortened_urls')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.shortened_id:
+            self.shortened_id = ShortenedUrl.generate_unique_id()
+        super().save(*args, **kwargs)
+
+    @staticmethod
+    def generate_unique_id():
+        length = 10
+        return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+    def __str__(self):
+        return f"{self.original_url} shortened to {self.shortened_id}"
