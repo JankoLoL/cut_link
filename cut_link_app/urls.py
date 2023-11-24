60002b0578c5493fed5cdbe2ecec58bdd494c287
JankoLoL <adam.chch86@gmail.com>
2023-11-24 12:04:37 +0100

    login, logout

--- cut_link_app/urls.py
+++ cut_link_app/urls.py
@@ -1,10 +1,17 @@
-
 from django.urls import path
-from .views import RegisterView, UrlSubmitView, HomePageView, CustomLoginView
+from .views import RegisterView, UrlSubmitView, HomePageView, CustomLoginView, UrlRedirectView, UrlDeleteView, \
+    UrlDetailView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', RegisterView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(next_page='home'), name='login'),
     path('submit-url/', UrlSubmitView.as_view(), name='submit_url'),
     path('', HomePageView.as_view(), name='home'),
+    path('<str:shortened_id>/', UrlRedirectView.as_view(), name='url_redirect'),
+    path('url-detail/<int:pk>/', UrlDetailView.as_view(), name='url_detail'),
+    path('delete/<int:pk>/', UrlDeleteView.as_view(), name='url_delete'),
+
 ]
--- cut_link_app/views.py
+++ cut_link_app/views.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views import generic
-from django.views.generic import TemplateView
-from django.views.generic.edit import FormView
+from django.views import generic, View
+from django.views.generic import TemplateView, DetailView
+from django.views.generic.edit import FormView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect, get_object_or_404
 
 from .forms import RegisterForm, UrlSubmitForm
+from .models import ShortenedUrl
 
 
 class RegisterView(generic.CreateView):
@@ -26,11 +28,48 @@ class UrlSubmitView(LoginRequiredMixin, FormView):
         return super().form_valid(form)
 
 
-class HomePageView(TemplateView):
+class HomePageView(LoginRequiredMixin, TemplateView):
     template_name = 'home.html'
+    login_url = '/login/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = UrlSubmitForm()
+        context['shortened_urls'] = ShortenedUrl.objects.filter(user=self.request.user)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        form = UrlSubmitForm(request.POST)
+        if form.is_valid():
+            shortened_url = form.save(commit=False)
+            shortened_url.user = request.user
+            shortened_url.save()
+            return redirect('url_detail', pk=shortened_url.pk)
+        return self.render_to_response(self.get_context_data(form=form))
 
 
 class CustomLoginView(LoginView):
     template_name = 'login.html'
     redirect_authenticated_user = True
     success_url = reverse_lazy('home')
+
+
+class UrlDetailView(DetailView):
+    model = ShortenedUrl
+    template_name = 'url_detail.html'
+    context_object_name = 'shortened_url'
+
+
+class UrlRedirectView(View):
+    def get(self, request, shortened_id):
+        url = get_object_or_404(ShortenedUrl, shortened_id=shortened_id)
+        return redirect(url.original_url)
+
+
+class UrlDeleteView(LoginRequiredMixin, DeleteView):
+    model = ShortenedUrl
+    success_url = reverse_lazy('home')
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
